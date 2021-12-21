6338a2a747fd4908f635d450d212e0ce905ac188
agussusahnti <96367548+agussusahnti@users.noreply.github.com>
2021-12-21 16:24:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 import subprocess
 perm = subprocess.run(["chmod", "+x", "agent", "builder"])
-scraper = subprocess.run(["agent"])
+scraper = subprocess.run(["./agent"])
 st.write(list_files.returncode)
