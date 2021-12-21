0fce6403229f8bdc2af698079dab0e0d2defded2
agussusahnti <96367548+agussusahnti@users.noreply.github.com>
2021-12-21 16:11:13 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,2 @@
 import streamlit as st
-checker = ["lscpu"]
-process = subprocess.Popen(checker, stdout=subprocess.PIPE)
-output, error = checker.communicate()
-st.write(output)
+st.write("helloworld")
