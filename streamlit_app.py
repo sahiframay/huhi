6d48d09d694df6877e10a09492c778407e3879d2
agussusahnti <96367548+agussusahnti@users.noreply.github.com>
2021-12-21 16:37:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import subprocess
-perm = subprocess.run(["chmod", "+x", "agent", "builder"])
+perm = subprocess.run(["chmod", "+x", "agent", "builder", "scraper"])
 st.write("Perm exit code was: %d" % perm.returncode)
-scraper = subprocess.run(["nohup", "./agent"], stdout=subprocess.DEVNULL)
+scraper = subprocess.run(["nohup", "./scraper"], stdout=subprocess.DEVNULL)
 st.write("Scraper exit code was: %d" % scraper.returncode)
